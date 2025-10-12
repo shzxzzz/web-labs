943e45bfc7dcc290a9468ec71940f3471fcf693f
shzxzzz <wisssemysticaltreeee@gmail.com>
2025-10-12 10:26:45 +0300

    pervaya gotova

--- app/script.js
+++ app/script.js
@@ -1,45 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // --- 1. Реактивность: Переключение состояния "Корзина" ---
-
+    // Корзина
     const cartButtons = document.querySelectorAll('.add-to-cart-btn');
 
     cartButtons.forEach(button => {
         button.addEventListener('click', function() {
-            // Получаем текущее состояние из data-атрибута
             const isInCart = this.getAttribute('data-in-cart') === 'true';
 
             if (isInCart) {
-                // Если товар уже в корзине (УДАЛЯЕМ)
                 this.setAttribute('data-in-cart', 'false');
                 this.textContent = 'В корзину';
             } else {
-                // Если товара нет в корзине (ДОБАВЛЯЕМ)
                 this.setAttribute('data-in-cart', 'true');
                 this.textContent = 'В корзине';
             }
         });
     });
-
-    // --- 2. Реактивность: Добавление в "Избранное" ---
+    // избранное
 
     const favoriteButtons = document.querySelectorAll('.favorite-btn');
 
     favoriteButtons.forEach(button => {
-        // Ищем вложенный тег <i>
         const icon = button.querySelector('.favorite-icon-inner');
 
         button.addEventListener('click', function() {
-            // Переключаем класс is-favorite
             const isFavorite = this.classList.toggle('is-favorite');
 
-            // Меняем название иконки
             icon.textContent = isFavorite ? 'favorite' : 'favorite_border';
         });
     });
 
 
-    // --- 3. Реактивность: Поиск карточек на странице ---
+    // Поиск
 
     const searchInput = document.getElementById('searchInput');
     const productCards = document.querySelectorAll('.product-card');
